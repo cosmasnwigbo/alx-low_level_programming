6be3dbf40958443922b6242e3730467d387899d8
cosmasnwigbo <cosmasnwigbo@gmail.com>
2022-06-08 05:55:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+  /**
+   * print phrase with proper grammar 
+   * outcome is art
+   * return 0
+   */
+   int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+

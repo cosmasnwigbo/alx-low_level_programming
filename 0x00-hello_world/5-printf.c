a2fb89cb87461b980a36585241859bbaecb58c68
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-10 07:34:53 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include	<stdio.h>
+/**
+  *	main	-	print	a	string	statement
+  *	Return:	0
+  **/
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return	(0);
+}

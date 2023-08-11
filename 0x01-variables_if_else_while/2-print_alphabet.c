7d0027efcdf48f3694f140be512d241bf8d6c524
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-11 09:09:49 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include	<stdio.h>
+/*
+ * main	-	Entry	point
+ *
+ * Return:	Always	0	(success)
+ *
+ */
+
+int	main(void)
+{
+	char	k	=	'a';
+
+	while	(k	<=	'z')
+	{
+		putchar(ch);
+		k++;
+	}
+	return	(0);
+}

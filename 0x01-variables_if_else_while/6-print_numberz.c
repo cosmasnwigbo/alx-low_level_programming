5bffa54ceec44f8d27a21d795382c5e9e2f762b9
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-11 12:00:01 -0700

    ipdate

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
 
 int	main(void)
 {
-	 int	x	=	0;
+	int	x	=	0;
 
-	 while	(x	<=	9)
-	 {
-		 putchar(x	+	'0');
-		 x++;
-	 }
-	 putchar('\n');
-	 return	(0);
+	while	(x	<=	9)
+	{
+		putchar(x	+	'0');
+		x++;
+	}
+	putchar('\n');
+	return	(0);
 }
 

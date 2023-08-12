16f72729654a229d2940c0f1e2f9a386d8d84d91
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-12 10:31:41 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include	<stdio.h>
+
+/**
+  *	main	-	function	print	all	combination	of
+  *	single	digits
+  *	Return:	0
+  **/
+
+int	main(void)
+{
+	int	x	=	0;
+
+	while	(x	<=	9)
+	{
+		putchar(x	+	'0');
+		if	(x	<	9)
+		{
+			putchar(',');
+		}
+		putchar(' ');
+		x++;
+	}
+	return	(0);
+}

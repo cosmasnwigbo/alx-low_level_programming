99a8d425bba2e2c1a7aefa1ab0ae47578fdc13ee
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-13 09:36:08 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include	<stdio.h>
+#include	<unistd.h>
+
+/*
+ * main - prints void
+ *
+ * return: 1 (success)
+ */
+int	main(void)
+{
+	fputs("and	that	piece	of	art	is	useful\"	-	Dora	korpar,	2015-10-19\n",	stdout);
+	return	(1);
+}

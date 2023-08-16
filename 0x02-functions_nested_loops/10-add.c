f2117c293eb01298bf07df078254300f5dc6c11c
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-16 06:33:19 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include	"main.h"
+/**
+  * add - add two integers
+  * @m: parameter for function add
+  * @n: parameter for function add
+  * Return: 0
+  *
+  **/
+
+int	add(int	n,	int	m)
+{
+	int	sum;
+
+	sum	=	(n	+	m);
+
+	return	(sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * times_table - print  the 9 times table starting with 0
+  * Return: 0
+  *
+  **/
+void times_table(void)
+{
+	int	j,	k;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 10;  k++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
+

fb264cc69d8ca5c730757b6f215a001cf5e09b64
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-18 04:34:34 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - multiply to integers
+  * @a: mul parameters to multiply
+  * @b: mul parameterto multiply
+  * Return: product
+  **/
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = (a * b);
+	return (product);
+}

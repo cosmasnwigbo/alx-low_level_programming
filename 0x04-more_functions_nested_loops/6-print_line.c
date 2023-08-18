a988da261124b11e4596f980f8022af25ef4adec
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-18 06:07:14 -0700

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_line - a function that draws a straight line
+  * @n: parameter for function print_line
+  * fellowed by a new line
+  **/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (x = 1; x < n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}

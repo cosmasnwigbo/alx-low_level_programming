d58d2f3a08a20702bc28584e4f5ba33934fb6d0a
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-18 07:11:53 -0700

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,28 @@
 
 void print_triangle(int size)
 {
-	int i;
-	int j;
-
-	if ( size <= 0)
-		_putchar('\n');
+	int i, j;
 	
-	for (i = 0; i <= size; i++)
+	if ( size <= 0)
 	{
-		for (j = 0; j <= i; j++)
-		{
-			if ((j + i) < size)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('#');
-			}
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j <= size; j++)
+			{
+				if ((i + j) < size)
+				{
+					_putchar('*');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }

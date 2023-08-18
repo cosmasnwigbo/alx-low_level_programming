d208b5d80fafd3f07a6aaa6b5db3ffd2cdbf637b
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-18 10:48:46 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,11 @@ int main(void)
 		}
 		else if ((i % 3 != 0) && (i % 5 == 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			if (i != 100)
+			{
+				printf(" ");
+			}
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{

cae62d9cfa85190914de6ffc53d1653c236223b9
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-21 09:30:16 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - update the value of a pointer to 98
+  *@n: parameter to functin reset_to_98
+  * fellow by a new line
+  **/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+

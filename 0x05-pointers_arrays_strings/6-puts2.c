e958880189a09eeeef97a9b1e3b7617a08a03aef
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-22 09:18:09 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-  * puts_half - function that print every other character of a string
-  *@: str parameter of function put
+  * puts2 - function that print every other character of a string
+  *@str: parameter of function put2
   *fellowed by a new line
   **/
 
 void puts2(char *str)
 {
 	int i;
-	for (i = 0; str[i] != '\0'; i+=2)
+
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		_putchar(str[i]);
 	}

398158f4f59f47a6bdfc1ff59b6cdc4f19fb4801
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-08-28 18:32:13 +0100

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		src[i] = dest[i];
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
-	return (0);
+	return (dest);
 }

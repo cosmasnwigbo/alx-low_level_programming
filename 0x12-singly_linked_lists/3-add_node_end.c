2a197af96720931bb9b84c30d705ca34ac2a4739
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-09-22 15:08:45 +0100

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,8 +26,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL || str == NULL)
+	if (new_node == NULL)
 		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
+
 	if (*head == NULL)
 	{
 		new_node = *head;

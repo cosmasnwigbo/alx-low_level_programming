ccf32f6acd36f39b46a19429a62d0e8c793403f9
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-09-22 12:45:36 +0100

    update

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t  *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+	new_node =malloc (sizeof(list_t));
+	{
+		if (new_node == NULL)
+		{
+			return (0);
+		}
+		new_node->str = strdup(str);
+		new_node->len = len;
+		new_node->next = *head;
+	}
+	return (new_node);
+
+	
+}
+
